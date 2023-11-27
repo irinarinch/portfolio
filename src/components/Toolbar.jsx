4d8c766d89bcb304d1918e56054c2ac01968cdcc
irinarinch <you@example.com>
2023-11-27 15:33:35 +0800

    short filter code

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,35 +1,35 @@
 import { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default class Toolbar extends Component { 
-  getItems() {    
-    const items = [];    
-    const item = this.props.filters.map((item) => 
-      item === this.props.selected 
-      ? <li key={item} className="nav-item selected" onClick={this.props.onSelectFilter}>{item}</li> 
-      : <li key={item} className="nav-item" onClick={this.props.onSelectFilter}>{item}</li>
-    );
+export default class Toolbar extends Component {
+  getItems() {
+    const items = [];
+    const item = this.props.filters.map((item) => (
+      <li
+        key={item}
+        className={`nav-item ${item === this.props.selected ? `selected` : ``}`}
+        onClick={this.props.onSelectFilter}
+      >
+        {item}
+      </li>
+    ));
 
     items.push(item);
 
     return items;
-  } 
+  }
 
   render() {
-    return (   
+    return (
       <nav className="toolbar">
-        <ul className="nav-items">          
-          {this.getItems()}
-        </ul>
-      </nav>  
+        <ul className="nav-items">{this.getItems()}</ul>
+      </nav>
     );
-  }  
+  }
 }
 
 Toolbar.propTypes = {
   filters: PropTypes.array,
   selected: PropTypes.string,
   onSelectFilter: PropTypes.func,
-}
-
-
+};
